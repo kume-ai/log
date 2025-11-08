4aac9ec4ed393f16e24c8bdca7d2e4d0ef82f1e0
kume-ai <kume-ai@users.noreply.github.com>
2025-11-08 18:42:54 +0000

    deploy: 743d3004bcf241f664865243d4c6b6bc26336727

--- assets/js/data/swconf.js
+++ assets/js/data/swconf.js
@@ -1,6 +1,6 @@
 const swconf = {
   
-    cacheName: 'chirpy-1762626670',resources: [
+    cacheName: 'chirpy-1762627374',resources: [
       '/assets/css/jekyll-theme-chirpy.css',
       '/',
       
