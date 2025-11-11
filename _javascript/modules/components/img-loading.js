99b93a58fc4d606091ec75f509b4cabe9d7e1a53
kume-ai <lu.3u8y@gmail.com>
2025-11-12 01:34:54 +0900

    js

--- _javascript/modules/components/img-loading.js
+++ _javascript/modules/components/img-loading.js
@@ -2,29 +2,7 @@
  * Setting up image lazy loading and LQIP switching
  */
 
-const ATTR_DATA_SRC = 'data-src';
-const ATTR_DATA_LQIP = 'data-lqip';
 
-const cover = {
-  SHIMMER: 'shimmer',
-  BLUR: 'blur'
-};
-
-function removeCover(clzss) {
-  this.parentElement.classList.remove(clzss);
-}
-
-function handleImage() {
-  if (!this.complete) {
-    return;
-  }
-
-  if (this.hasAttribute(ATTR_DATA_LQIP)) {
-    removeCover.call(this, cover.BLUR);
-  } else {
-    removeCover.call(this, cover.SHIMMER);
-  }
-}
 
 /**
  * Switches the LQIP with the real image URL.
@@ -47,11 +25,11 @@ export function loadImg() {
   });
 
   // Images loaded from the browser cache do not trigger the 'load' event
-  //document.querySelectorAll('article img[loading="lazy"]').forEach((img) => {
-    //if (img.complete) {
-      //removeCover.call(img, cover.SHIMMER);
-    //}
-  //});
+  document.querySelectorAll('article img[loading="lazy"]').forEach((img) => {
+    if (img.complete) {
+      removeCover.call(img, cover.SHIMMER);
+    }
+  });
 
   // LQIPs set by the data URI or WebP will not trigger the 'load' event,
   // so manually convert the URI to the URL of a high-resolution image.
@@ -65,3 +43,4 @@ export function loadImg() {
     });
   }
 }
+
